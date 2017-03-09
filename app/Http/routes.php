5b03a1b95059dc4dc1676f882753078c76ef62dd
Nadia <nadia.wahid28@gmail.com>
2017-03-09 15:49:04 -0800

    Tugas 2 Selesai

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,5 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/Nadia',function(){
-	echo "Nadia"
+	echo "Nadia";
+});
+Route::get('Hello-World',function(){
+	return 'Hello World';
+});
+Route::get('pengguna/{pengguna}',function($pengguna){
+	return "Hello-World dari pengguna $pengguna";
+});
+Route::get('kelasd2/framework/{mhs?}',function($mhs="Anonim"){
+	return "Selamat Datang $mhs";
 });
